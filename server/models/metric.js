a048773f66c3493adfe0e615dca5f93fdd377d9d
bot2do <63306828+bot2do@users.noreply.github.com>
2020-06-26 12:49:09 +0530

    Update metric.js

--- server/models/metric.js
+++ server/models/metric.js
@@ -23,13 +23,15 @@ var hourly = mongoose.model('Parameter',primaryDB, 'hourly_primary');
 var daily = mongoose.model('Parameter',primaryDB, 'daily_primary');
 var weekly = mongoose.model('Parameter',primaryDB, 'weekly_primary');
 var monthly = mongoose.model('Parameter',primaryDB, 'monthly_primary');
+var clientConfigs = mongoose.model('Parameter',primaryDB, 'client_configs');
 
 module.exports = {
     minute : minute,
     hourly : hourly,
     daily : daily,
     weekly : weekly,
-    monthly : monthly   
+    monthly : monthly,
+    clientConfigs: clientConfigs   
 };
 
 
