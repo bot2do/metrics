8e05709c3905701b8d58b84398d1156cbebb2978
bot2do <63306828+bot2do@users.noreply.github.com>
2020-06-29 11:51:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
+const cors = require('cors');
 
 
 var config = require('./config/properties');
@@ -25,6 +26,15 @@ app.use(log);
 app.use(bodyParserJSON);
 app.use(bodyParserURLEncoded);
 app.use(cookieParser());
+app.use(cors());
+app.options('*', cors());
+
+router.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Control-Expose-Headers", "X-My-Custom-Header, X-Another-Custom-Header");
+  next(); // make sure we go to the next routes and don't stop here
+});
 
 app.use('/api',router);
 
